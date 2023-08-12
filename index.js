ced2adf6a75d0b1cd0c1c7b6e8875a75bc0b5590
Utkarsh1504 <utkarshmishra73444@gmail.com>
2023-08-12 10:42:48 +0530

    static hosting

--- index.js
+++ index.js
@@ -7,32 +7,29 @@ import morgan from "morgan";
 import cors from "cors";
 import path from "path";
 
-config();
 const __filename = new URL(import.meta.url).pathname;
 const __dirname = path.dirname(__filename);
+config();
 
 // server connection
 const server = express();
 
 // db connection
-// mongoose.connect('mongodb://localhost:27017/ecommerceDB')
 const main = async () => {
-  await mongoose.connect(
-    "mongodb+srv://umishra1504:hUGkDXx7swMpf8GG@dbcluster.6jrwtpz.mongodb.net/ecommerceDB2?retryWrites=true&w=majority"
-  );
+  await mongoose.connect(process.env.MONGO_URL);
   console.log("db connected!");
 };
 main().catch((err) => console.log(err));
 
 server.use(cors());
-server.use(express.static(path.join(__dirname, process.env.PUBLIC_DIR)));
 server.use(morgan("dev"));
 server.use(express.json());
+server.use(express.static(path.resolve(__dirname, process.env.PUBLIC_DIR)));
 server.use(express.urlencoded({ extended: true }));
 server.use("/products", productRouter);
 server.use("/users", userRouter);
 server.use("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
 
 server.listen(process.env.PORT, (req, res) => {
